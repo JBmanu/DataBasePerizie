bc90e49abc602bc1efc3d8178e6bf583cbfddcea
Bmanuel <buizomanuel@gmail.com>
2021-07-18 16:25:24 +0200

    query insert sinistri

---
+++ insertSinistri_Manca_Descrizione_IDAssicurato_Associato_a_Assicurazione_e_CategoriaSinistro.sql
@@ -0,0 +1,195 @@
+USE [Teleperizie]
+GO
+
+INSERT INTO [dbo].[Sinistri]
+           ([Assicurazione]
+           ,[Descrizione]
+           ,[Data]
+           ,[Categoria_Sinistro]
+           ,[ID_Luogo]
+           ,[ID_Studio]
+           ,[ID_Assicurato])
+     VALUES
+           ('Interdum Ligula Institute'
+           ,'Ubriaco va a sbattere'
+           ,'2021-07-13', 'R.C.A'
+           , 123, 101, 135), 
+
+		   ('Mattis Incorporated'
+           ,''
+           ,'2021-10-13', 'Furto'
+           , 103, 101, 137), 
+
+		   ('Mauris Magna Duis Corporation'
+           ,'Ubriaco va a sbattere'
+           ,'2021-01-13', 'R.C.A'
+           , 104, 1, 236), 
+
+		   ('Magna Sed Consulting'
+           ,'Distruggono casa'
+           ,'2021-12-13', 'Atti vandalici '
+           , 105, 2, 428), 
+
+		   ('Luctus Vulputate Incorporated'
+           ,'Ubriaco va a sbattere'
+           ,'2021-13-13', 'R.C.A'
+           , 106, 5, 135), 
+
+		   ('Leo Cras Corporation'
+           ,'Ubriaco va a sbattere'
+           ,'2020-07-13', 'R.C.A'
+           , 107, 4, 135), 
+
+		   ('Laoreet Lectus Quis Inc.'
+           ,'Ubriaco va a sbattere'
+           ,'2019-07-13', 'R.C.A'
+           , 108, 6, 135), 
+
+		   ('Interdum Nunc Company'
+           ,'Ubriaco va a sbattere'
+           ,'2018-10-13', 'R.C.A'
+           , 109, 10, 135), 
+
+		   ('Integer Id Limited'
+           ,'Ubriaco va a sbattere'
+           ,'2019-07-13', 'R.C.A'
+           , 110, 10, 135), 
+
+		   ('In At Consulting'
+           ,'Ubriaco va a sbattere'
+           ,'2018-07-13', 'R.C.A'
+           , 111, 14, 135), 
+
+		   ('Imperdiet LLP'
+           ,'Ubriaco va a sbattere'
+           ,'2017-07-13', 'R.C.A'
+           , 112, 15, 135), 
+
+		   ('Hendrerit Donec Porttitor Corporation'
+           ,'Ubriaco va a sbattere'
+           ,'2021-09-13', 'R.C.A'
+           , 113, 101, 135), 
+
+		   ('Fermentum Metus Consulting'
+           ,'Ubriaco va a sbattere'
+           ,'2018-02-13', 'R.C.A'
+           , 114, 5, 135), 
+
+		   ('Eu LLC'
+           ,'Ubriaco va a sbattere'
+           ,'2017-07-13', 'R.C.A'
+           , 115, 12, 135), 
+
+		   ('Eu Consulting'
+           ,'Ubriaco va a sbattere'
+           ,'2018-07-13', 'R.C.A'
+           , 116, 51, 135), 
+
+		   ('Eu Corp.'
+           ,'Ubriaco va a sbattere'
+           ,'2021-05-21', 'R.C.A'
+           , 117, 61, 135), 
+
+		   ('Et Euismod Et Corp.'
+           ,'Ubriaco va a sbattere'
+           ,'2021-07-21', 'R.C.A'
+           , 118, 51, 135), 
+
+
+		   ('Et Eros Proin Company'
+           ,'Ubriaco va a sbattere'
+           ,'2021-07-20', 'R.C.A'
+           , 119, 71, 135), 
+
+		   ('Eros Non Corporation'
+           ,'Ubriaco va a sbattere'
+           ,'2021-07-31', 'R.C.A'
+           , 120, 85, 135), 
+
+		   ('Eros Nec Tellus Foundation'
+           ,'Ubriaco va a sbattere'
+           ,'2018-07-21', 'R.C.A'
+           , 121, 61, 135), 
+
+		   ('Erat Volutpat Nulla Associates'
+           ,'Ubriaco va a sbattere'
+           ,'2019-09-23', 'R.C.A'
+           , 122, 92, 135),
+
+		   ('Enim Industries'
+           ,'Ubriaco va a sbattere'
+           ,'2019-07-24', 'R.C.A'
+           , 125, 55, 135), 
+
+		   ('Enim Etiam Associates'
+           ,'Ubriaco va a sbattere'
+           ,'2021-12-13', 'R.C.A'
+           , 126, 36, 135), 
+
+		   ('Duis Ac Arcu Limited'
+           ,'Ubriaco va a sbattere'
+           ,'2020-03-13', 'R.C.A'
+           , 127, 68, 135), 
+
+		   ('Dolor Quam Elementum PC'
+           ,'Ubriaco va a sbattere'
+           ,'2020-07-23', 'R.C.A'
+           , 128, 89, 135), 
+
+		   ('Dolor Elit Inc.'
+           ,'Ubriaco va a sbattere'
+           ,'2020-01-21', 'R.C.A'
+           , 129, 35, 135), 
+
+		   ('Dolor Company'
+           ,'Ubriaco va a sbattere'
+           ,'2020-03-27', 'R.C.A'
+           , 130, 75, 135), 
+
+		   ('Dictum Cursus Nunc PC'
+           ,'Ubriaco va a sbattere'
+           ,'2020-03-10', 'R.C.A'
+           , 131, 36, 135), 
+
+		   ('Curabitur Egestas Nunc Company'
+           ,'Ubriaco va a sbattere'
+           ,'2019-02-19', 'R.C.A'
+           , 132, 100, 135), 
+
+		   ('Cras Vulputate Associates'
+           ,'Ubriaco va a sbattere'
+           ,'2015-07-13', 'R.C.A'
+           , 133, 93, 135), 
+
+		   ('Bibendum Sed Institute'
+           ,'Ubriaco va a sbattere'
+           ,'2016-08-23', 'R.C.A'
+           , 134, 23, 135), 
+
+		   ('Augue Consulting'
+           ,'Ubriaco va a sbattere'
+           ,'2021-08-23', 'R.C.A'
+           , 135, 21, 135), 
+
+		   ('Auctor Odio Limited'
+           ,'Ubriaco va a sbattere'
+           ,'2016-07-27', 'R.C.A'
+           , 136, 5, 135), 
+		   
+		   ('Auctor Non Feugiat Inc.'
+           ,'Ubriaco va a sbattere'
+           ,'2020-07-26', 'R.C.A'
+           , 137, 18, 135), 
+
+		   ('Arcu Morbi LLC'
+           ,'Ubriaco va a sbattere'
+           ,'2019-03-21', 'R.C.A'
+           , 138, 101, 135), 
+
+		   ('Ante Ipsum Institute'
+           ,'Ubriaco va a sbattere'
+           ,'2021-07-10', 'R.C.A'
+           , 139, 101, 135)
+GO
+
+
